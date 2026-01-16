d6351aad6144beb06a4bf200aadde2e8524bf9d1
griesel050-web <griesel050@gmail.com>
2026-01-16 02:08:20 +0200

    Create vote.js

---
+++ script/vote.js
@@ -0,0 +1,29 @@
+const voteSites = [
+  { name: "MCRank", url: "https://mcrank.com/server/nexoria" },
+  { name: "Crafty", url: "https://crafty.gg/servers/play.nexoriarealm.xyz" },
+  { name: "MinecraftServers.org", url: "https://minecraftservers.org/server/682443" },
+  { name: "Minecraft Server List", url: "https://minecraft-server-list.com/server/517241/" },
+  { name: "Minecraft Menu", url: "https://minecraft.menu/server-nexoria.5696" },
+  { name: "MC ServerTime", url: "https://mcservertime.com/server-nexoria.2784" },
+  { name: "MinecraftServers.me", url: "https://minecraftservers.me/server/nexoria.22037" },
+  { name: "MCList", url: "https://mclist.io/server/67761-play-nexoriarealm-xyz-survival-realmip-nexor/vote" },
+  { name: "Minecraft Buzz", url: "https://minecraft.buzz/server/18581" },
+  { name: "Play Minecraft Servers", url: "https://play-minecraft-servers.com/minecraft-servers/nexoria/" },
+  { name: "TopMinecraftServers", url: "https://topminecraftservers.org/vote/42550" }
+];
+
+const grid = document.getElementById("voteGrid");
+
+voteSites.forEach(site => {
+  const card = document.createElement("div");
+  card.className = "vote-card";
+
+  card.innerHTML = `
+    <h3>${site.name}</h3>
+    <a href="${site.url}" target="_blank" rel="noopener noreferrer">
+      Vote Now
+    </a>
+  `;
+
+  grid.appendChild(card);
+});
