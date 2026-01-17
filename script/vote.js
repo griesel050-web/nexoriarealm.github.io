12db23963ceb7382943f59bffd1468b44332aea7
griesel050-web <griesel050@gmail.com>
2026-01-17 06:14:07 +0200

    Update vote.js

--- script/vote.js
+++ script/vote.js
@@ -9,8 +9,18 @@ const voteSites = [
   { name: "MCList", url: "https://mclist.io/server/67761-play-nexoriarealm-xyz-survival-realmip-nexor/vote" },
   { name: "Minecraft Buzz", url: "https://minecraft.buzz/server/18581" },
   { name: "Play Minecraft Servers", url: "https://play-minecraft-servers.com/minecraft-servers/nexoria/" },
-  { name: "TopMinecraftServers", url: "https://topminecraftservers.org/vote/42550" }
+  { name: "TopMinecraftServers", url: "https://topminecraftservers.org/vote/42550" },
+
+  // Newly added
+  { name: "Minecraft-ServerList", url: "https://minecraft-serverlist.com/server/4104" },
+  { name: "MC-Server-List", url: "https://mc-server-list.com/server/2463-nexoria/" },
+  { name: "CraftList", url: "https://craftlist.org/nexoria#vote" },
+  { name: "MineList", url: "https://minelist.net/server/5110" },
+  { name: "MCLike", url: "https://mclike.com/minecraft-server-193517" },
+  { name: "Minecraft-Server.net", url: "https://minecraft-server.net/details/Nexoria/" },
+  { name: "Minecraft IP List", url: "https://www.minecraftiplist.com/server/Nexoria-39119" }
 ];
+
 
 const grid = document.getElementById("voteGrid");
 
